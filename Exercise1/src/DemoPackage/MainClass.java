a032bc60091771df4e85995fd92d4499c63543c4
Osvin Mazon Guzman <sergio.mazon@hotmail.com>
2023-01-14 12:08:54 -0600

    update readme

---
+++ Exercise1/src/DemoPackage/MainClass.java
@@ -0,0 +1,14 @@
+package DemoPackage;
+
+public class MainClass {
+    public static void main(String[] args) {
+
+
+        int number=80;
+        long longNumber=20L;
+        double doubleNumber=2.5;
+        boolean booleanType= true;
+
+        System.out.println("int="+number+"\n"+"long="+longNumber+"\n"+"double="+doubleNumber+"\n"+"boolea="+booleanType);
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # Delivery-exercises-topic-1
  
+Create a java project from scratch.
+Create a package.
+Inside the package create a class.
+Inside the class create a main method
+Print all types of data seen.
+Most common data types:
+int, long, double, boolean, String
